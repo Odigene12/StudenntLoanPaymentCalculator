f21847aaff4c80968305bdcaea218c55d1bed73b
Odigene Joseph <odigene12@gmail.com>
2022-01-09 21:18:14 -0600

    Initial Commit/Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'studentLoanPayCalc-app';
+  title = 'Student Loan Payment Calculator';
 }
